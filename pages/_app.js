278aafddd60347256d9f252b188eb5e12c17fa21
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-23 16:21:28 +0530

    installed vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import { wrapper, store } from "../redux/store";
 import { Provider } from "react-redux";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
         <Component {...pageProps} />
+        <Analytics />
       </Provider>
     </>
   );
